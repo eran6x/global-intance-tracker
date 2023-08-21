4635c94ab782a7ed81408d922a163fca1ce202b7
Eran Amir <eran.amir@gmail.com>
2023-08-21 15:15:41 +0100

    iterate on all EC2 and RDS

--- tracker.py
+++ tracker.py
@@ -1,80 +1,109 @@
 ####
-# retrieve all instances from AWS account
+# retrieve all EC2 instances from AWS account
 # 1) list all running instnaces in all regions along with their owner and tags
-# 2) list all running databases, 
-# 3) list all used EBS, and then other resources.
+# 2) list all running RDS databases with tags
+# 3) write results to respective CSV files.
 
+import configparser
 import csv
 import itertools
-import configparser
 import os
 import time
-import jmespath
+import json
 import boto3
-import html
+import jmespath
 
-max_results = 10 
+MAX_RESULTS = 10
+PATH_TO_CREDENTIALS_JSON = '/Users/eran/projects/AWS/ERAN-AWS-cred'
 
-# Get list of profiles
-config = configparser.ConfigParser()
-path = os.path.join(os.path.expanduser('~'), '/Users/eran/projects/AWS/ERAN-AWS-cred')
-config.read(path)
-profiles = config.sections()
 
-# Get list of regions
-ec2_client = boto3.client('ec2')
-regions = [region['RegionName']
-            for region in ec2_client.describe_regions()['Regions']]
+def main():
+    # Get list of profiles
+    config = configparser.ConfigParser()
+    path = os.path.join(os.path.expanduser('~'), PATH_TO_CREDENTIALS_JSON)
+    config.read(path)
+    profiles = config.sections()
 
-# Get list of EC2 attributes from all profiles and regions
-myData = []
-for profile in profiles:
+    # look for EC2: 
+    client_name = 'ec2'
+    b_client = boto3.client(client_name)
+    regions = get_list_of_regions(b_client)
+    my_data = describe_ec2_instances(regions, profiles)
+
+    # #prepare output file
+    timestr = time.strftime("%Y%m%d-%H%M%S")
+    outputfile = f"ec2-inventory-{timestr}.csv"
+    write_ec2_data_to_file(outputfile, my_data)
+
+    # look for RDS: 
+    my_data = describe_rds_instances(regions)
+
+    #prepare output file
+    timestr = time.strftime("%Y%m%d-%H%M%S")
+    outputfile = f"rds-inventory-{timestr}.csv"
+    write_rds_data_to_file(outputfile, my_data)
+
+
+def write_ec2_data_to_file(outputfile, my_data):
+    # Write my_data to CSV file with headers
+    output = list(itertools.chain(*my_data))
+    with open(outputfile, "w", newline="", encoding="UTF") as f:
+        writer = csv.writer(f)
+        writer.writerow(['Type','State','Name','AZ','InstanceID','PublicIP','KeyPair', 'tags'])
+
+        #Write each line to file.
+        for line in output:
+            if line[3] == 'running':
+                newline2 = [line[2], line[3], line[8], line[4], line[6], line[7], "N/A"]
+                writer.writerow(newline2)
+    f.close
+
+def write_rds_data_to_file(outputfile, my_data):
+    # Write my_data to CSV file with headers
+    with open(outputfile, "w", newline="", encoding="UTF") as f:
+        f.write("AZ, InstanceID , TagList\n")
+        for item in my_data:
+            f.write(item + "\n")
+    f.close
+
+
+"""Use Boto3 to enumerate all regions"""
+def get_list_of_regions(b_client):
+    regions = [region['RegionName']
+    for region in b_client.describe_regions()['Regions']]
+    return regions
+
+
+def describe_ec2_instances(regions, profiles):
+    # Get list of EC2 attributes from all profiles and regions
+    my_data = []
+    for profile in profiles:
+        for region in regions:
+            current_session = boto3.Session(profile_name = profile, region_name = region)
+            client = current_session.client('ec2')
+            response = client.describe_instances()
+            output = jmespath.search("Reservations[].Instances[].[NetworkInterfaces[0].OwnerId, InstanceId, InstanceType, \
+                State.Name, Placement.AvailabilityZone, PrivateIpAddress, PublicIpAddress, KeyName, [Tags[?Key=='Name'].Value] [0][0]]", response)
+            my_data.append(output)
+    return my_data
+
+
+def describe_rds_instances(regions):
+    my_data = []
+
+    # Loop through the regions and list the RDS instances in each region
     for region in regions:
-        current_session = boto3.Session(profile_name = profile, region_name = region)
-        client = current_session.client('ec2')
-        response = client.describe_instances()
-        output = jmespath.search("Reservations[].Instances[].[NetworkInterfaces[0].OwnerId, InstanceId, InstanceType, \
-            State.Name, Placement.AvailabilityZone, PrivateIpAddress, PublicIpAddress, KeyName, [Tags[?Key=='Name'].Value] [0][0]]", response)
-        myData.append(output)
+        # Create a boto3 client for the region
+        rds_client = boto3.client('rds', config=boto3.session.Config(region))
 
-#prepare output file
-timestr = time.strftime("%Y%m%d-%H%M%S")
-outpufile = f"ec2-inventory-{timestr}.csv"
+        # List the RDS instances in the region
+        response = rds_client.describe_db_instances()
+        if (len(response['DBInstances']) > 0 ):
+            for instance in response['DBInstances']:
+                line = region  + ',' + instance['DBInstanceIdentifier'] + ',' + json.dumps(instance['TagList'])
+                my_data.append(line)
+    return my_data
 
-# Write myData to CSV file with headers
-output = list(itertools.chain(*myData))
-with open(outpufile, "w", newline="") as f:
-    writer = csv.writer(f)
-    writer.writerow(['Type','State','Name','AZ','InstanceID','PublicIP','KeyPair', 'tags'])
-
-    #Write each line to file.
-    for line in output:
-        if line[3] == 'running':
-            #if row status is Running
-            #   write to list only: (['Type','State','Name','AZ','InstanceID','PublicIP','KeyPair'])
-            #   newline = f"{line[2]}, {line[3]}, {line[8]}, {line[4]}, {line[6]}, {line[7]}"
-            #print(newline)
-            # get the tags:
-            # Call the describe_tags method to retrieve the tags for the specified instance
-            response = get_instance_tags(current_session, line[4],line[1])
-            client = current_session.client('ec2', region_name=line[4])
-            instance_id = line[1]
-            response = client.describe_tags(InstanceId=instance_id)
-            response = client.describe_tags(
-                InstanceId=instance_id,
-                Filters={
-                    'key': {'FilterType': 'KeyEquals', 'Value': 'owner'},
-                    'value': {'FilterType': 'StringLike', 'Value': '%myTagValue%'}
-                },
-                MaxResults=max_results
-            )
-            
-            response = "no tags"
-            print(response)
-            newline2 = [line[2], line[3], line[8], line[4], line[6], line[7], response]
-            writer.writerow(newline2)
-
-create_html_file(output)
 
 def get_regions_list():
     # Get list of regions
@@ -83,46 +112,6 @@ def get_regions_list():
                 for region in ec2_client.describe_regions()['Regions']]
     return regions
 
-def create_html_file(lst):
-    # Get the maximum widths for each column
-    max_widths = {}
-    for i, item in enumerate(lst):
-        for key, value in item.items():
-            if key not in max_widths:
-                max_widths[key] = len(str(value))
-            else:
-                max_widths[key] = max(max_widths[key], len(str(value)))
-    
-    # Create the table header row
-    header_row = "<tr>" + "".join([f"<th style='{k}: {max_widths[k]}px'>{k}</th>" for k in lst[0].keys()]) + "</tr>"
-    
-    # Create the body rows
-    body_rows = []
-    for row in range(1, len(lst)):
-        body_row = "<tr>" + "".join([f"<td style='{k}: {max_widths[k]}px'>{item[k]}]</td>" for k in lst[0].keys()]) + "</tr>"
-        body_rows.append(body_row)
-    
-    # Combine everything into one string
-    return f"<table>{header_row}<tbody>{ (',').join(body_rows)}</tbody></table>"
 
-
-
-def get_instance_tags(current_session, line[4],line[1]):
-    client = current_session.client('ec2', region_name=line[4])
-    instance_id = line[1]
-    response = client.describe_tags(InstanceId=instance_id)
-    response = client.describe_tags(
-        InstanceId=instance_id,
-        Filters={
-            'key': {'FilterType': 'KeyEquals', 'Value': 'owner'},
-            'value': {'FilterType': 'StringLike', 'Value': '%myTagValue%'}
-        },
-        MaxResults=max_results
-    )
-
-def describe_instance_tags(ec2client, instance_id):
-   response = ec2client.describe_tags(InstanceId=instance_id)
-    ##ec2_id = output[0][]
-    ec2client = boto3.client('ec2')
-    instance_id = output[0][1]
-    tagstring = describe_instance_tags(instance_id)
+if __name__ == '__main__':
+  main()
